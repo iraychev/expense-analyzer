3d7c51c45a7189fd8bb9c6f446b997efc6f5096a
Ivaylo Raychev <92424190+iraychev@users.noreply.github.com>
2025-02-24 17:48:05 +0200

    enable security

--- src/main/java/com/iraychev/expenseanalyzer/security/AuthController.java
+++ src/main/java/com/iraychev/expenseanalyzer/security/AuthController.java
@@ -1,28 +1,28 @@
-//package com.iraychev.expenseanalyzer.security;
-//
-//import com.iraychev.expenseanalyzer.exception.ResourceNotFoundException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@Slf4j
-//@RequiredArgsConstructor
-//public class AuthController {
-//    private final AuthService authService;
-//
-//    @PostMapping("/api/token")
-//    public String token(HttpServletRequest request) {
-//        try {
-//            String authHeader = request.getHeader("Authorization");
-//            log.debug("Authorization Header: {}", authHeader);
-//
-//            return authService.authenticateAndGenerateToken(authHeader);
-//        } catch (Exception e) {
-//            log.error("Authentication failed", e);
-//            throw new ResourceNotFoundException("User Not Found");
-//        }
-//    }
-//}
+package com.iraychev.expenseanalyzer.security;
+
+import com.iraychev.expenseanalyzer.exception.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class AuthController {
+   private final AuthService authService;
+
+   @PostMapping("/api/token")
+   public String token(HttpServletRequest request) {
+       try {
+           String authHeader = request.getHeader("Authorization");
+           log.debug("Authorization Header: {}", authHeader);
+
+           return authService.authenticateAndGenerateToken(authHeader);
+       } catch (Exception e) {
+           log.error("Authentication failed", e);
+           throw new ResourceNotFoundException("User Not Found");
+       }
+   }
+}
