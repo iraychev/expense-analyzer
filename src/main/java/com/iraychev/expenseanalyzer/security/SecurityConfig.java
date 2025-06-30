d74163981b9e6a1614646ee974f5b55c73cbd52d
Ivaylo Raychev <ivoraychev1337@gmail.com>
2025-06-30 14:33:33 +0300

    fix register endpoint

--- src/main/java/com/iraychev/expenseanalyzer/security/SecurityConfig.java
+++ src/main/java/com/iraychev/expenseanalyzer/security/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig {
                )
                .authorizeHttpRequests(auth -> auth
                        .requestMatchers(HttpMethod.POST, "/api/v1/token").permitAll()
+                       .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
                        .requestMatchers(
                                 antMatcher("/swagger-ui.html"),
                                 antMatcher("/swagger-ui/**"),
